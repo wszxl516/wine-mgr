14d06f44da3de4ee11b08ee38b7dcfb27a36f74a
Zhang <wszxl516@163.com>
2023-03-06 20:20:45 +0800

    fix exec program

--- src/config/env.rs
+++ src/config/env.rs
@@ -5,28 +5,18 @@ use std::collections::HashMap;
 use std::ffi::{CStr, CString};
 use std::fmt::{Display, Formatter};
 
-const DEFAULT_ENV: [(&str, &str); 14] = [
-                ("WINEPREFIX", "/opt/windows/windows7"),
-                ("WINEARCH", "win64"),
-                ("WINE_LARGE_ADDRESS_AWARE","1"),
-                ("DXVK_HUD", "1"),
-                //("WINEDEBUG", "warn+all"),
-                ("DISPLAY", ":0.0"),
-                (
-                    "Path",
-                    r"c:\\windows;c:\\windows\\system;c:\\windows\\syswow64;c:\\windows\\system32",
-                ),
-                ("WINEDLLOVERRIDES", "d3d10core,d3d11,d3d12,d3d9,d3dcompiler_33,d3dcompiler_34,d3dcompiler_35,d3dcompiler_36,d3dcompiler_37,d3dcompiler_38,d3dcompiler_39,d3dcompiler_40,d3dcompiler_41,d3dcompiler_42,d3dcompiler_43,d3dcompiler_46,d3dcompiler_47,d3dx10,d3dx10_33,d3dx10_34,d3dx10_35,d3dx10_36,d3dx10_37,d3dx10_38,d3dx10_39,d3dx10_40,d3dx10_41,d3dx10_42,d3dx10_43,d3dx11_42,d3dx11_43,d3dx9_24,d3dx9_25,d3dx9_26,d3dx9_27,d3dx9_28,d3dx9_29,d3dx9_30,d3dx9_31,d3dx9_32,d3dx9_33,d3dx9_34,d3dx9_35,d3dx9_36,d3dx9_37,d3dx9_38,d3dx9_39,d3dx9_40,d3dx9_41,d3dx9_42,d3dx9_43,dxgi,nvapi,nvapi64,nvml=n;winemenubuilder="),
-                //("MANGOHUD","1"),
-                //("MANGOHUD_DLSYM","1"),
-                ("DRI_PRIME","1"),
-                ("__NV_PRIME_RENDER_OFFLOAD","1"),
-                ("__GLX_VENDOR_LIBRARY_NAME","nvidia"),
-                ("__VK_LAYER_NV_optimus","NVIDIA_only"),
-                ("VK_ICD_FILENAMES", "/usr/share/vulkan/icd.d/nvidia_icd.json"),
-                ("DXVK_ENABLE_NVAPI", "1"),
-                ("WINE", "/usr/bin/wine"),
-            ];
+const DEFAULT_ENV: [(&str, &str); 6] = [
+    ("WINEPREFIX", "/opt/windows/windows7"),
+    ("WINEARCH", "win64"),
+    ("DXVK_HUD", "1"),
+    //("WINEDEBUG", "warn+all"),
+    ("DISPLAY", ":0.0"),
+    (
+        "Path",
+        r#"c:\\windows;c:\\windows\\system;c:\\windows\\syswow64;c:\\windows\\system32"#,
+    ),
+    ("VK_ICD_FILENAMES", ""),
+];
 
 const ENV_KEY: &str = "env";
 
--- src/executor/executor.rs
+++ src/executor/executor.rs
@@ -1,13 +1,28 @@
 use super::super::config::Env;
+use log::info;
 use nix::unistd;
 use std::ffi::{CStr, CString};
+use std::io::Write;
 use std::os::unix::io::AsRawFd;
 
 pub fn executor(program: String, args: Vec<String>, envs: Env, log: String, show_log: bool) {
+    let env_str = envs
+        .get_map()
+        .iter()
+        .map(|(k, v)| format!("{}={}", k, v))
+        .collect::<Vec<String>>()
+        .join(" ");
     match show_log {
-        true => {}
+        true => {
+            info!("Environment: {}\n", env_str);
+            info!("Command: {}\n", args.join(" "));
+        }
         false => {
-            let fp = std::fs::File::create(log).unwrap();
+            let mut fp = std::fs::File::create(log).unwrap();
+
+            fp.write_fmt(format_args!("Environment: {}\n", env_str)).unwrap();
+            fp.write_fmt(format_args!("Command: {}\n", args.join(" ")))
+                .unwrap();
             let raw_fd = fp.as_raw_fd();
             unistd::dup2(raw_fd, 1).unwrap();
             unistd::dup2(raw_fd, 2).unwrap();
@@ -17,7 +32,7 @@ pub fn executor(program: String, args: Vec<String>, envs: Env, log: String, show
         .iter()
         .map(|s| CString::new(s.as_bytes()).unwrap().into_boxed_c_str())
         .collect::<Vec<Box<CStr>>>();
-    unistd::execve(
+    unistd::execvpe(
         CString::new(program).unwrap().as_c_str(),
         args.as_slice(),
         envs.cstr().as_slice(),
